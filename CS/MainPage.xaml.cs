d4f45237b336835965471f16222557d454b6b8e8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Windows.Controls;
+
+namespace RichEditCustomFontsSL {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            DevExpress.Utils.Internal.FontManager.RegisterFontFamily("Monotype Corsiva",
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("RichEditCustomFontsSL.Fonts.mtcorsva.ttf"));
+            richEditControl1.Document.DefaultCharacterProperties.FontName = "Monotype Corsiva";
+            richEditControl1.ApplyTemplate();
+        }
+    }
+}
